e9d3ee175fd7c22eea2816aad1a0574c99280b23
Luiz Filipe da Silva <luizfilipe.tj@icloud.com>
2020-06-07 07:10:49 -0300

    Update minor version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='lets-debug',
-    version='0.0.2',
+    version='0.0.3',
     url='https://github.com/luizfilipezs/lets-debug',
     license='MIT License',
     author='Luiz Filipe da Silva',
