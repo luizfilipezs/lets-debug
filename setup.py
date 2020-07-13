75255ace53854383c83b2395047360a9d05116f0
Luiz Filipe da Silva <luizfilipe.tj@icloud.com>
2020-07-13 15:37:40 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup
 
-with open("README.md", "r") as fh:
+with open('README.md', 'r') as fh:
     long_description = fh.read()
 
 setup(
@@ -14,7 +14,7 @@ setup(
     keywords='debug tools terminal',
     description=u'Useful tools for debugging Python code',
     long_description=long_description,
-    long_description_content_type="text/markdown",
+    long_description_content_type='text/markdown',
     packages=['lets_debug'],
     install_requires=[],
     python_requires='>=3.7.5',
